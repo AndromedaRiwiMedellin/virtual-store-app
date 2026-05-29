fcb72bf19cc0042b72f514b21b14bd9a200a88d9
cadena0 <cadenajuanjose68@gmail.com>
2026-05-28 19:49:23 -0500

    add: google outh in url

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
         {/* 2. ENVOLVEMOS EL <App /> CON EL PROVEEDOR DE GOOGLE */}
         {/* Recuerda cambiar luego el clientId por el tuyo real de Google Console */}
-        <GoogleOAuthProvider clientId="15639194860-94g33vmsmna1ffp8ietkifrcc7pvh9uo.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId="15639194860-bpc0p32b9mdnvdodeg9h30eqauqdma2q.apps.googleusercontent.com">
             <App />
         </GoogleOAuthProvider>
     </StrictMode>,
